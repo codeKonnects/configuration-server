8142bc15f8601f62c4867cab6912b520bacc2924
codeKonnects <topedaves@gmail.co>
2022-10-25 01:22:31 +0100

    implements cloud config server

---
+++ src/test/java/io/codoKonnects/cloudconfigserver/CloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.codoKonnects.cloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
